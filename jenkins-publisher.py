9e2958c720a373324768f186866f32e4f5d1ff15
LarsKoelbel <136699088+LarsKoelbel@users.noreply.github.com>
2025-10-02 16:06:05 +0200

    Changed replacement methode

--- jenkins-publisher.py
+++ jenkins-publisher.py
@@ -27,7 +27,7 @@ def make_new_version(path: str, version: str = None):
                 continue
             
             # Place the version
-            data.replace('##VERSION##', version)
+            data = data[:index] + version + data[(index + 11):]
 
             print(data)
 
